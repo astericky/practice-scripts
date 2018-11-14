1e4d0a0f5c69ec000513530f02c142510307b487
Chris Sanders <astericky@gmail.com>
2018-11-14 15:08:52 -0800

    add scripts

---
+++ get-words-with-letters.js
@@ -0,0 +1,31 @@
+const words = ['eat', 'meat', 'heat', 'chris', 'too', 'lead', 'hot', 'bar', 'mead', 'head', 'read', 'peets bar']
+let letters = {
+  'a': [], 'e': []
+}
+
+console.log(function wordsWith(letters, words) {
+  words.forEach(word => {
+    word.split('').forEach(letter => {
+      if (letters[letter]) {
+        letters[letter].push(word)
+      }
+    })
+  })
+
+  Object.keys(letters).map
+}(letters, words))
+
+// console.log plus function that takes a string letters
+// and an array of strings that takes an array of words 
+console.log(function getWorldsWith(letters, words) {
+  // TODO: come up with a way to build function 
+  // without side effects
+  letters.split('').forEach(letter => {
+    words = words.filter(word => word.includes(letter))
+  })
+
+  return words
+}('eat', words))
+
+
+
---
+++ hangman.js
@@ -0,0 +1,50 @@
+let wrongGuesses = 3
+let word = ''
+let wordBlank = []
+
+const setupGame = () => {
+  word = 'apple'
+  wrongGuesses = 3;
+  wordBlank = word.split('').map(() => '_');
+}
+
+const updateGame = (letter) => {
+  let index = word.indexOf(letter)
+
+  while (index !== -1) {
+    wordBlank[index] = letter
+    index = word.indexOf(letter, index + 1)
+  }
+  console.log(wordBlank.join())
+}
+
+const isEndOfGame = () => {
+  if (word === wordBlank.join('')) {
+    console.log('You WON!')
+  } else if (wrongGuesses === 0) {
+    console.log('You LOST!')
+  }
+  console.log('Keep Going!')
+}
+
+const chooseAndCheckLetter = (letter) => {
+  if (word.includes(letter)) {
+    updateGame(letter)
+    console.log('Letter exists.')
+  } else {
+    wrongGuesses = wrongGuesses - 1
+    console.log('Letter does not exist.')
+  }
+  isEndOfGame()
+}
+
+setupGame()
+chooseAndCheckLetter('p')
+chooseAndCheckLetter('l')
+chooseAndCheckLetter('x')
+chooseAndCheckLetter('a')
+chooseAndCheckLetter('v')
+chooseAndCheckLetter('e')
+// chooseAndCheckLetter('y') // This is used for a test.
+
+
